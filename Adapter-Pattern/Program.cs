cfa554553e17e665a0aae9f0545e990bc5a253fb
nimishmadan98 <nimishmadan98@gmail.com>
2024-06-13 22:56:22 +0530

    add code for adapter pattern

---
+++ Adapter-Pattern/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Adapter_Pattern;
+
+public interface IPaymentProcessor{
+    void ProcessPayment(decimal amount);
+}
+
+public class LegacyPaymentProcessor{
+    public void ProcessPayment(double amount){
+        Console.WriteLine($"Payment of {amount} made through legacy system.");
+    }
+}
+
+public class PaymentAdapter: IPaymentProcessor{
+    private readonly LegacyPaymentProcessor _legacyPaymentProcessor;
+    public PaymentAdapter(LegacyPaymentProcessor legacyPaymentProcessor){
+        _legacyPaymentProcessor = legacyPaymentProcessor;
+    }
+
+    public void ProcessPayment(decimal amount){
+        _legacyPaymentProcessor.ProcessPayment((double)amount);
+    }
+}
+
+public class PaymentService{
+    private readonly IPaymentProcessor _paymentProcessor;
+
+    public PaymentService(IPaymentProcessor paymentProcessor){
+        _paymentProcessor = paymentProcessor;
+    }
+
+    public void MakePayment(decimal amount){
+        _paymentProcessor.ProcessPayment(amount);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        LegacyPaymentProcessor legacyPaymentProcessor = new LegacyPaymentProcessor();
+        IPaymentProcessor paymentProcessor = new PaymentAdapter(legacyPaymentProcessor);
+        PaymentService paymentService = new PaymentService(paymentProcessor);
+        paymentService.MakePayment(12.12M);
+    }
+}
