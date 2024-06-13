b1c48e487fc66329ca19b8a8467268385cc341a5
nimishmadan98 <nimishmadan98@gmail.com>
2024-06-13 22:57:13 +0530

    add comments

--- Adapter-Pattern/Program.cs
+++ Adapter-Pattern/Program.cs
@@ -1,4 +1,22 @@
-﻿namespace Adapter_Pattern;
+﻿/*
+
+The Adapter Design Pattern is a structural pattern that allows objects with incompatible interfaces to work together.
+It acts as a bridge between two incompatible interfaces by wrapping one of the objects with a new interface that the
+other object can work with.
+
+Key Concepts
+Target: Defines the domain-specific interface that the Client uses.
+Client: Collaborates with objects that implement the Target interface.
+Adaptee: An existing interface that needs adapting.
+Adapter: Implements the Target interface and wraps the Adaptee object to translate the request from the Client to the Adaptee.
+
+Real-Life Industry Example
+Consider a scenario where a legacy system is used to process payments but a new system has been introduced with a different
+interface. The new system needs to integrate with the old payment processing system.
+
+*/
+
+namespace Adapter_Pattern;
 
 public interface IPaymentProcessor{
     void ProcessPayment(decimal amount);
